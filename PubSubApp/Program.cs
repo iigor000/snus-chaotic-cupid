284b6fe07f24d952727d22b4a3975f1ec4f2d7c3
iigor000 <igor.novakovic2004@gmail.com>
2026-06-02 20:33:25 +0200

    Cleaned up unneccesary swagger config

--- PubSubApp/Program.cs
+++ PubSubApp/Program.cs
@@ -2,20 +2,10 @@ using ChaoticCupid.PubSubApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
 app.MapHub<MessageHub>("/messageHub");
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.Run();
