6ccbfd09e2264d6b0dc59f6585e98f8ada3f87e0
iigor000 <igor.novakovic2004@gmail.com>
2026-06-02 19:17:15 +0200

    Added solution file

--- PubSubApp/MessageHub.cs
+++ PubSubApp/MessageHub.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ChaoticCupid.PubSubApp
 {
--- Sub/Program.cs
+++ Sub/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace ChaoticCupid.Sub
